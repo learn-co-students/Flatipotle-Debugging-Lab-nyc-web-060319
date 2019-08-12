7edf4ca150e3c4827837059a554f601d5dc623d5
Alexander Riccio <test35965+learn@gmail.com>
2019-08-12 09:51:15 -0400

    solve

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {
+  getByLabelText,
+  getByText,
+  getByTestId,
+  queryByTestId,
+  // Tip: all queries are also exposed on an object
+  // called "queries" which you could import here as well
+  wait,
+} from '@testing-library/dom'
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+// it('renders items', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, div);
+//   const container = document.getElementById("root");
+//   const whiteRice = getByLabelText(container, "White Rice");
+
+//   ReactDOM.unmountComponentAtNode(div);
+
+// }
+// )
